d64b82161179917d5aaafe31e1b15925d51151cf
bsv <bsv@encode.dk>
2021-02-11 08:11:00 +0200

    + BalancedBrackets problem

---
+++ src/main/java/com/sbondar/hackerrank/BalancedBrackets.java
@@ -0,0 +1,54 @@
+package com.sbondar.hackerrank;
+
+import java.util.*;
+
+
+//https://www.hackerrank.com/challenges/balanced-brackets/problem
+
+public class BalancedBrackets {
+    public static void main(String[] args) {
+        new BalancedBrackets().processStrings();
+    }
+
+    public void processStrings() {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.nextLine();
+        for (int l = 0; l < n; l++) {
+            String s = scan.nextLine();
+            System.out.println(isBalanced(s));
+        }
+    }
+
+    private String isBalanced(final String s) {
+        Map<Character, Character> br = new HashMap<>();
+        br.put('{', '}');
+        br.put('(', ')');
+        br.put('[', ']');
+
+        Deque<Character> openBr = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (br.containsKey(c)) {
+                openBr.push(c);
+            } else {
+                if (openBr.isEmpty() || br.get(openBr.pop()) != c) {
+                    return "NO";
+                }
+            }
+        }
+        return openBr.isEmpty() ? "YES" : "NO";
+    }
+}
+
+/*
+1
+{[(])}
+
+1
+}}}
+
+3
+{[()]}
+{[(])}
+{{[[(())]]}}
+* */
